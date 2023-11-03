47b2d2d047861e92dbcad738756fad18212b61a5
sheilamb7 <sheilamb7@gmail.com>
2023-11-03 13:18:29 +0100

    prompt_item, onClick, preventDefault

--- src/showPrompts/prompt_item.jsx
+++ src/showPrompts/prompt_item.jsx
@@ -51,7 +51,7 @@ export default class PromptItem extends Component {
     }
 
     handleOpenModal(e) {
-        e.preventDefault();
+        e.preventDefault;
         console.log('handnel open modal')
         this.setState({
             promptModalisOpen: true
@@ -91,7 +91,7 @@ export default class PromptItem extends Component {
                 prompt_content={this.state.prompts_content}
                 // inputBook={this.addBook}
                 />
-            <div className='prompt_container' onClick={(e) => {this.handleOpenModal()}}>
+            <div className='prompt_container' onClick={() => {this.handleOpenModal()}}>
                 {this.state.prompts_completed_book ? (
                 <div className='prompt_name prompt_complete'>
                     {this.state.prompts_content}
