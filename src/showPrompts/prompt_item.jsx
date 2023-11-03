6ff4ee059a4ad68fe456253620d280da046ece8a
sheilamb7 <sheilamb7@gmail.com>
2023-11-03 12:40:57 +0100

    prompt_item, added binds

--- src/showPrompts/prompt_item.jsx
+++ src/showPrompts/prompt_item.jsx
@@ -24,6 +24,8 @@ export default class PromptItem extends Component {
             promptModalisOpen: false
         }
         
+        this.handleOpenModal = this.handleOpenModal.bind(this);
+        this.handleModalClose = this.handleModalClose.bind(this);
     }
 
 
@@ -87,7 +89,7 @@ export default class PromptItem extends Component {
                 prompt_content={this.state.prompts_content}
                 // inputBook={this.addBook}
                 />
-            <div className='prompt_container' onClick={() => this.handleOpenModal()}>
+            <div className='prompt_container' onClick={this.handleOpenModal}>
                 {this.state.prompts_completed_book ? (
                 <div className='prompt_name prompt_complete'>
                     {this.state.prompts_content}
